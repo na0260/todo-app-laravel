f2a68e0dabc94f4973f73c5ea16e2acc8cf93b19
na0260 <samjr.008@gmail.com>
2024-09-07 22:19:34 +0600

    todo form component added

---
+++ resources/views/components/todo-form.blade.php
@@ -0,0 +1,12 @@
+<form action="{{ $action === 'create' ? route('todos.store') : route('todos.update', $todo['id']) }}" method="POST" class="mb-6">
+    @csrf
+    @if ($action === 'edit')
+        @method('PUT')
+    @endif
+    <div class="flex items-center space-x-2">
+        <input type="text" name="title" value="{{ $todo->title ?? '' }}" placeholder="Enter your todo" class="border-gray-300 rounded w-full p-2" required>
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            {{ $action === 'create' ? 'Add Todo' : 'Update Todo' }}
+        </button>
+    </div>
+</form>
